1ef2d4b4cb6c7af286805e8da37db9fab5b18852
Clarissa T <53034464+CurePeach@users.noreply.github.com>
2022-11-08 19:59:59 +1100

    Added final list.c

--- week09/list.c
+++ week09/list.c
@@ -68,16 +68,59 @@ struct node *copy(struct node *list) {
 }
 
 struct node *list_append(struct node *first_list, struct node *second_list) {
-    return NULL;
+    struct node *first_copy = copy(first_list);
+    struct node *second_copy = copy(second_list);
+
+    if (first_copy == NULL) {
+        return second_copy;
+    }
+
+    struct node *first_last = find_last_node(first_list);
+    first_last->next = second_copy;
+
+    return first_copy;
 }
 
 int identical(struct node *first_list, struct node *second_list) {
-    return 5841;
+    struct node *first_curr = first_list;
+    struct node *second_curr = second_list;
+
+    while (first_curr != NULL && second_curr != NULL) {
+        if (first_curr->data != second_curr->data) {
+            return 0;
+        }
+
+        first_curr = first_curr->next;
+        second_curr = second_curr->next;
+    }
+
+    if (first_curr != NULL || second_curr != NULL) {
+        return 0;
+    }
+
+    return 1;
 }
 
 struct node *set_intersection(
     struct node *first_list,
     struct node *second_list
 ) {
-    return NULL;
+    struct node *new = NULL;
+    struct node *first_curr = first_list;
+    struct node *second_curr = second_list;
+    
+    while (first_curr != NULL) {
+        while (second_curr != NULL) {
+            if (first_curr->data == second_curr->data) {
+                new = add_last(new, first_curr->data);
+                second_curr = NULL;
+            }
+
+            second_curr = second_curr->next;
+        }
+
+        first_curr = first_curr->next;
+    }
+
+    return new;
 }
